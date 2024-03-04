7f7e9af90dd9efec712219bb56e5b40eb8fadda9
kidmortal <kidmortal@gmail.com>
2024-03-03 21:28:12 -0300

    update port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*', allowedHeaders: '*' });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
