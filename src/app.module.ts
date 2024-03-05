f01c3219bd87bfd01baa30dbc821d751d644897e
kidmortal <kidmortal@gmail.com>
2024-03-05 14:08:01 -0300

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
   imports: [FirebaseModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
