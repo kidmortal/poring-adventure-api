28615cf7397c8e09a0a6b0a41152d4710ccf305c
kidmortal <kidmortal@gmail.com>
2024-03-06 09:32:02 -0300

    add some throttle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { UsersModule } from './users/users.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { ItemsModule } from './items/items.module';
 import { MarketModule } from './market/market.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [FirebaseModule, UsersModule, ItemsModule, MarketModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 30000,
+        limit: 60,
+      },
+    ]),
+    FirebaseModule,
+    UsersModule,
+    ItemsModule,
+    MarketModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
