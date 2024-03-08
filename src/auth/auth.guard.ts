be39fc231eb58afc7dced2cb82ae981a634a1ee2
kidmortal <kidmortal@gmail.com>
2024-03-08 20:54:41 -0300

    add some websocket routes

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -11,7 +11,6 @@ export class AuthGuard implements CanActivate {
   constructor(private readonly firebaseRepository: FirebaseRepository) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    console.log('guard');
     const request = context.switchToHttp().getRequest();
     const headerEmail = request.headers['authenticated_email'];
     if (headerEmail) {
