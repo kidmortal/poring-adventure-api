3c0102f54d78554b771b449ed2dbdc49a50907e7
kidmortal <kidmortal@gmail.com>
2024-03-08 20:21:05 -0300

    enough

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -3,10 +3,11 @@ import { MarketService } from './market.service';
 import { MarketController } from './market.controller';
 import { ItemsModule } from 'src/items/items.module';
 import { UsersModule } from 'src/users/users.module';
+import { MarketGateway } from './market.gateway';
 
 @Module({
   imports: [ItemsModule, UsersModule],
   controllers: [MarketController],
-  providers: [MarketService],
+  providers: [MarketService, MarketGateway],
 })
 export class MarketModule {}
