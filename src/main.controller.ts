8700ff5761871587250375272d82eb25ac108e2b
kidmortal <kidmortal@gmail.com>
2024-03-13 10:57:02 -0300

    add api version

--- src/main.controller.ts
+++ src/main.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
+import { execSync } from 'child_process';
 
 @Controller('')
 export class MainController {
@@ -9,5 +10,14 @@ export class MainController {
     const indexPath = path.join(__dirname, '..', 'public', 'index.html');
     const indexContent = fs.readFileSync(indexPath, 'utf-8');
     return indexContent;
+  }
+
+  @Get('/version')
+  getApiVersion() {
+    const branchData = execSync('git rev-parse HEAD').toString();
+    const revision = branchData.trim();
+    return {
+      api_version_hash: revision,
+    };
   }
 }
