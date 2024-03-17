08164b8ffa2b8fca820e48e69b0088af6901f834
kidmortal <kidmortal@gmail.com>
2024-03-17 17:00:06 -0300

    migrations

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,8 +4,8 @@ import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { createClient } from '@libsql/client';
 
 const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  url: `${process.env.TURSO_DATABASE_URL ?? 'file:dev.db'}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN ?? ''}`,
 });
 
 const adapter = new PrismaLibSQL(libsql);
