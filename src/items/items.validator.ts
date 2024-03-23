5bd823695f844eab92abf5e4b2bf135ee7c0bc4b
kidmortal <kidmortal@gmail.com>
2024-03-23 11:43:56 -0300

    add transaction to equip item

---
+++ src/items/items.validator.ts
@@ -0,0 +1,21 @@
+import { BadRequestException } from '@nestjs/common';
+import { EQUIPABLE_CATEGORIES } from './entities/categories';
+
+export const ItemsValidator = {
+  isEquippable: (args: { category: string }) => {
+    if (!EQUIPABLE_CATEGORIES.includes(args.category)) {
+      throw new BadRequestException('This item is not equipable');
+    }
+  },
+
+  isSameCategory: (args: {
+    categoryItem: string;
+    categoryEquipped: string;
+  }) => {
+    if (args.categoryItem === args.categoryEquipped) {
+      throw new BadRequestException(
+        'You cannot equip two of the same kind of equipment',
+      );
+    }
+  },
+};
