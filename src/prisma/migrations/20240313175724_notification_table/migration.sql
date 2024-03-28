3338fe4705feb00bf303cb2698a6948980b914b7
kidmortal <kidmortal@gmail.com>
2024-03-28 20:40:11 -0300

    squash all migrations

--- src/prisma/migrations/20240313175724_notification_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Notification" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "content" TEXT NOT NULL,
-    "visualized" BOOLEAN NOT NULL,
-    "userId" INTEGER NOT NULL,
-    CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
