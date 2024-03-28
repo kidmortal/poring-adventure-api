b821ed290f7fd74980fef2415b9eb651b2370d42
kidmortal <kidmortal@gmail.com>
2024-03-28 17:56:46 -0300

    stable

--- src/guild/guild.module.ts
+++ src/guild/guild.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GuildService } from './guild.service';
 import { GuildGateway } from './guild.gateway';
 import { CacheModule } from '@nestjs/cache-manager';
+import { WebsocketModule } from 'src/websocket/websocket.module';
 
 @Module({
-  imports: [CacheModule.register({ ttl: 1000 * 60 * 10 })], // 10 minutes cache
+  imports: [WebsocketModule, CacheModule.register({ ttl: 1000 * 60 * 10 })], // 10 minutes cache
   providers: [GuildGateway, GuildService],
 })
 export class GuildModule {}
