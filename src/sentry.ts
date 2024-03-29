3ca33e58932a1a8a5be870d1f0704cf86f03bea6
kidmortal <kidmortal@gmail.com>
2024-03-28 21:39:52 -0300

    prevent sentry logs from dev to be production

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,6 +1,8 @@
 import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
+console.log(process.env.NODE_ENV);
+
 export function setupSentry() {
   Sentry.init({
     dsn: 'https://d313533768ee7fcdae023276ee6b7bd9@o1086666.ingest.us.sentry.io/4506963275481088',
@@ -9,5 +11,6 @@ export function setupSentry() {
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
     // Set sampling rate for profiling - this is relative to tracesSampleRate
     profilesSampleRate: 1.0,
+    environment: process.env.NODE_ENV,
   });
 }
