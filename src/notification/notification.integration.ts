308f5feaf0224ab44fd028ee576a70cb2f3be563
kidmortal <kidmortal@gmail.com>
2024-04-11 20:03:24 -0300

    fix notification to all users

--- src/notification/notification.integration.ts
+++ src/notification/notification.integration.ts
@@ -18,38 +18,57 @@ export class OneSignalNotificationService {
   constructor() {}
 
   async sendPushNotification(args: { message: string }) {
-    const result = await this.client.post('', {
-      name: 'test_notification_name',
-      headings: { en: "Gig'em Ags" },
-      contents: {
-        en: args.message,
-        pt: args.message,
+    const success = await this._postNotification({
+      body: {
+        app_id: process.env.ONESIGNAL_APP_ID,
+        name: 'test_notification_name',
+        headings: { en: "Gig'em Ags" },
+        small_icon: 'notification_icon',
+        large_icon: 'notification_icon',
+        contents: {
+          en: args.message,
+          pt: args.message,
+        },
+        included_segments: ['All'],
       },
-      included_segments: ['All'],
     });
-    console.log(result);
+    if (success) {
+      this.logger.debug(`Notification sent to all users`);
+    }
   }
 
   async sendPushNotificationToUser(args: {
     userEmail: string;
     message: string;
   }) {
-    try {
-      await this.client.post('', {
+    const success = await this._postNotification({
+      body: {
         app_id: process.env.ONESIGNAL_APP_ID,
         name: `Notification for ${args.userEmail}`,
         headings: { en: `Custom Notification` },
         target_channel: 'push',
+        small_icon: 'notification_icon',
+        large_icon: 'notification_icon',
         contents: {
           en: args.message,
           pt: args.message,
         },
         include_aliases: { external_id: [args.userEmail] },
-      });
+      },
+    });
+    if (success) {
       this.logger.debug(`Notification sent to ${args.userEmail}`);
+    }
+  }
+
+  async _postNotification(args: { body: object }): Promise<boolean> {
+    try {
+      await this.client.post('', args.body);
+      return true;
     } catch (error) {
       Sentry.captureException(error);
       this.logger.warn(error.response.data.errors);
+      return false;
     }
   }
 }
