c8583e9dcc555e8c95dd3c0b524f09f17dfaded0
kidmortal <kidmortal@gmail.com>
2024-04-12 10:10:48 -0300

    update memory info to number

--- src/utils.ts
+++ src/utils.ts
@@ -39,24 +39,9 @@ function getLevelFromExp(exp: number) {
   return level;
 }
 
-function formatMemory(memory) {
-  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
-
-  let index = 0;
-  let value = memory;
-
-  while (value >= 1024 && index < units.length - 1) {
-    value /= 1024;
-    index++;
-  }
-
-  return `${value.toFixed(2)} ${units[index]}`;
-}
-
 export const utils = {
   isSuccess,
   getRandomNumberBetween,
   getLevelFromExp,
   randomDamage,
-  formatMemory,
 };
