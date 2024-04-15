55619bd08fdfbea3f7cf6b45ea3815540269fe2c
kidmortal <kidmortal@gmail.com>
2024-04-15 17:23:23 -0300

    store related stuff 2

--- src/purchase/revenuecat.service.ts
+++ src/purchase/revenuecat.service.ts
@@ -1,17 +1,25 @@
 import axios from 'axios';
 
+import * as Sentry from '@sentry/node';
 export class RevenueCatService {
   private client = axios.create({
     baseURL: 'https://api.revenuecat.com/v1/',
     headers: {
       Authorization: `Bearer ${process.env.REVENUE_CAT_REST_API_KEY}`,
+      'Content-Type': 'application/json',
     },
   });
   constructor() {}
   async refundPurchase(args: { transactionId: string; appUserId: string }) {
-    const result = await this.client.post(
-      `https://api.revenuecat.com/v1/subscribers/${args.appUserId}/transactions/${args.transactionId}/refund`,
-    );
-    return result;
+    try {
+      await this.client.post(
+        `https://api.revenuecat.com/v1/subscribers/${args.appUserId}/transactions/${args.transactionId}/refund`,
+        {},
+      );
+      return true;
+    } catch (error) {
+      Sentry.captureException(error);
+      return false;
+    }
   }
 }
