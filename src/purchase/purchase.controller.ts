06683d0782b75795ea00087b496d9c2be96af8d6
kidmortal <kidmortal@gmail.com>
2024-04-15 18:40:16 -0300

    add generic exception filter

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, UseFilters } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 import { PurchaseGuard } from './purchase.guard';
 import { RevenueCatPurchaseWebhook } from './entities/purchase.entity';
+import { HttpExceptionFilter } from './http-exception.filter';
 
+@UseFilters(HttpExceptionFilter)
 @Controller('purchase')
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
