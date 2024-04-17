d3b0bad348ffedfd59596ccaca5a0021e7781a2b
kidmortal <kidmortal@gmail.com>
2024-04-17 20:06:44 -0300

    update api

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,17 +1,52 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
+import { createHash } from 'crypto';
+import { Socket } from 'socket.io';
 import { FirebaseRepository } from 'src/services/firebase/firebase.repository';
 
 @Injectable()
 export class AuthService {
   private logger = new Logger('AuthService');
   constructor(private readonly firebaseRepository: FirebaseRepository) {}
-  async getAuthenticatedEmailFromToken(token: string) {
-    if (!token) return false;
-    try {
-      const email = await this.firebaseRepository.validateEmail({ token });
-      return email;
-    } catch (error) {
-      this.logger.error(`Error on getAuthenticatedEmailFromToken`);
+
+  async validateWebsocketConnection(args: { socket: Socket }) {
+    const isDiscordToken = this._validateDiscordToken(args);
+    if (isDiscordToken) {
+      return true;
+    }
+    const isEmailToken = await this._validateEmailToken(args);
+    if (isEmailToken) {
+      return true;
+    }
+    return false;
+  }
+
+  private async _validateEmailToken(args: { socket: Socket }) {
+    const client = args.socket;
+    const accessToken = client.handshake.auth?.accessToken;
+    if (accessToken) {
+      try {
+        const email = await this.firebaseRepository.validateEmail({ token: accessToken });
+        client.handshake.auth.email = email;
+        this.logger.debug(`Websocket connected - ${email}`);
+        return true;
+      } catch (error) {
+        console.log(error);
+        throw new ForbiddenException('An invalid access token has been provided ');
+      }
+    }
+  }
+  private _validateDiscordToken(args: { socket: Socket }) {
+    const client = args.socket;
+    const accessToken = client.handshake.auth?.accessToken;
+
+    if (accessToken) {
+      const hash = createHash('md5').update(process.env.DISCORD_API_TOKEN).digest('hex');
+      if (accessToken === hash) {
+        const email = 'discord';
+        client.handshake.auth.email = email;
+        this.logger.debug(`Websocket connected - ${email}`);
+        return true;
+      }
       return false;
     }
   }
